8aa72359eb1bd258634fe39db0cc645c57f93ff3
laurekt <laurenvien2006@gmail.com>
2023-07-13 09:58:39 -0700

    stemspiration

--- STEMspiration/ContentView.swift
+++ STEMspiration/ContentView.swift
@@ -9,13 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack {
+            Color(.systemYellow)
+                .ignoresSafeArea()
+            
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("saraz")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                    .padding(15)
+                
+                HStack {
+                    Text("Sara Alfageeh")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(15)
+                    
+                    Text("Coder of Fantasy")
+                }
+                
+                
+                
+                Text("Co-founder of One More Multiverse, a video game company focused on creating the next generation of tabletop RPG games.")
+                    .padding(15)
+                
+            } .padding()
+            .background(Rectangle()
+                .foregroundColor(.white))
+            .cornerRadius(15)
+            .shadow(radius: 10)
+            .padding(15)
+            
         }
-        .padding()
+
+        
+        
+        
+        
+        
+        
     }
 }
 
